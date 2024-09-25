97d639862507cb5fe42518f4e42709f8a1777d41
neaurellia <catherineaurellia@gmail.com>
2024-09-25 11:53:27 +0700

    TUTORIAL 4 DONE

--- main/views.py
+++ main/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.shortcuts import reverse
 
 # Create your views here.
 
@@ -94,3 +94,26 @@ def logout_user(request):
 
 def main_view(request):
     return render(request, 'main.html')
+
+def edit_mood(request, id):
+    # Get mood entry based on id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry as an instance of the form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Save form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood based on id
+    mood = MoodEntry.objects.get(pk = id)
+    # Delete mood
+    mood.delete()
+    # Return to home page
+    return HttpResponseRedirect(reverse('main:show_main'))
